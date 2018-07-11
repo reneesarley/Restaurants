c41fabc98fe47879071041272da0af0480fe3bea
Student <example@example.com>
2018-07-11 11:47:05 -0700

    add Restaurant model and test files

---
+++ Restaurant.Tests/Models.Tests/Restaurant.Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DiningTracker.Models;
+using System.Collections.Generic;
+
+
+namespace DiningTracker.Tests
+{
+    [TestClass]
+    public class RestaurantTests : IDisposable
+    {
+        public void Dispose()
+        {
+            Restaurant.DeleteAll();
+        }
+
+        public RestaurantTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=diningTracker_test;";
+        }
+
+        //[TestMethod]
+        //public void GetAll_DbStartsEmpty_0()
+        //{
+        //    //Arrange
+        //    //Act
+        //    int result = Cuisine.GetAll().Count;
+
+        //    //Assert
+        //    Assert.AreEqual(0, result);
+        //}
+
+        //[TestMethod]
+        //public void Save_SavesToDatabase_ItemList()
+        //{
+        //    //Arrange
+        //    Cuisine newCuisine = new Cuisine("testCusine");
+
+        //    //Act
+        //    newCuisine.Save();
+        //    List<Cuisine> result = Cuisine.GetAll();
+        //    List<Cuisine> testList = new List<Cuisine> { newCuisine };
+
+        //    //Assert
+        //    CollectionAssert.AreEqual(result, testList);
+        //}
+    }
+}
---
+++ Restaurant/Models/Restaurant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace DiningTracker.Models
+{
+    public class Restaurant
+    {
+        private int _id;
+        private string _name;
+        private int _cusineId;
+
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM restaurants;";
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+   }
+}
