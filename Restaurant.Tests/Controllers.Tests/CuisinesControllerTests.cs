8ab80d0fc96cc4cf08312a14ee04c0a764e73a2e
Student <example@example.com>
2018-07-11 11:07:30 -0700

    add cuisine controller, view and controller test files

---
+++ Restaurant.Tests/Controllers.Tests/CuisinesControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DiningTracker.Controllers;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiningTracker.Tests
+{
+    [TestClass]
+    public class CuisinesControllerTests
+    {
+        [TestMethod]
+        public void AddNew_ReturnsCorrectView_True()
+        {
+            //Arrange
+            CuisinesController controller = new CuisinesController();
+
+            //Act
+            ActionResult addNewView = controller.AddNew();
+
+            //Assert
+            Assert.IsInstanceOfType(addNewView, typeof(ViewResult));
+        }
+
+    }
+}
