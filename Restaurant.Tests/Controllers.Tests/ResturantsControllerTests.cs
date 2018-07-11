b96e8ebe992f9e0502b9c963e693d1890f19416a
Student <example@example.com>
2018-07-11 14:00:35 -0700

    add views to add new restaurant and confirm new has been added

---
+++ Restaurant.Tests/Controllers.Tests/ResturantsControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DiningTracker.Controllers;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiningTracker.Tests
+{
+    [TestClass]
+    public class RestaurantsControllerTests
+    {
+
+        [TestMethod]
+        public void AddNew_ReturnsCorrectView_True()
+        {
+            //Arrange
+            RestaurantsController controller = new RestaurantsController();
+            //Act
+            ActionResult addNewView = controller.AddNew();
+            //Assert
+            Assert.IsInstanceOfType(addNewView, typeof(ViewResult));
+        }
+    }
+
+}
---
+++ Restaurant/Controllers/Restaurants.Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiningTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DiningTracker.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        [HttpGet("restaurants/addNew")]
+    public ActionResult AddNew()
+    {
+
+        return View();
+    }
+
+        [HttpPost("/saveRestaurant")]
+        public ActionResult AllRestaurants(string restaurantName, int cuisineId)
+        {
+            //Restaurant newRestaurant = new Restaurant(restaurantName, cuisineId);
+            //newRestaurant.Save();
+            //Restaurant.GetAll()
+            return View();
+        }
+    }
+}
